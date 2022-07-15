070bd05d603249be9906cc7fc403937282cc2a47
rubykoboi <dianne.robbi@safavieh.com>
2022-07-15 17:31:36 -0400

    fixing UI with a more stable layout...in the middle of it...under construction

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,9 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
@@ -12,16 +15,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 
+import javax.swing.BoxLayout;
 import javax.swing.DropMode;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.UIManager;
 import javax.swing.filechooser.FileFilter;
 
@@ -47,9 +49,13 @@ public class App {
 	final static String DESKTOP_PATH = System.getProperty("user.home") + "\\Desktop\\";
 	final static String FILE_EXTENSION = ".xlsx";
 	private JTextArea lblStatus;
-	
+	private static JPanel buttonsPanel;
+	private static JPanel mainPanel;
+	private static JPanel actionPanel;
+	private static JPanel bigPanel;
 	private static XSSFSheet sheet;
 	private static XSSFWorkbook workbook;
+	private static App window;
 
 	/**
 	 * Launch the application.
@@ -58,7 +64,7 @@ public class App {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					App window = new App();
+					window = new App();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -84,22 +90,13 @@ public class App {
 	 */
 	private void initialize() {
 		frame = new JFrame("Designs Build Checker");
-		frame.setBounds(100, 100, 450, 227);
+		frame.setBounds(100, 100, 500, 250);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		frame.getContentPane().setLayout(new BorderLayout());
 		
-		topLabel = new JLabel("List out designs or item IDs to check which customers have them built out.");
-		frame.getContentPane().add(topLabel);
-		
-		Panel panel_1 = new Panel();
-		frame.getContentPane().add(panel_1);
-		textAreaPane = new JScrollPane();
-		textArea = new JTextArea();
-		textArea.setDropMode(DropMode.INSERT);
-		textArea.setFont(new Font("Calibri Light", Font.PLAIN, 13));
-		textArea.setWrapStyleWord(true);
-		textArea.setLineWrap(true);
-		
+		// organize buttons in a Grid
+		buttonsPanel = new JPanel(new GridLayout(3,1,10,12));
+		buttonsPanel.setBackground(Color.blue);
 		btnSource = new JButton("Source");
 		btnSource.setToolTipText("Choose master sheet of item IDs (might be labeled 'Customer Builds.xlsx').");
 		btnImport = new JButton("Import");
@@ -108,55 +105,66 @@ public class App {
 		btnRun.setToolTipText("Process the input list of designs and/or item IDs.");
 		btnRun.setEnabled(false);
 		
-		bottomLabel = new JLabel("Separate IDs from each other by a comma (,).");
-		bottomLabel.setFont(new Font("Calibri Light", Font.PLAIN, 12));
-		
-		lblStatus = new JTextArea("test");
+		buttonsPanel.setSize(250,25);
+		buttonsPanel.add(btnSource);
+		buttonsPanel.add(btnImport);
+		buttonsPanel.add(btnRun);
+
+		// set source area
+		lblStatus = new JTextArea();
+		lblStatus.setSize(250, 250);
 		lblStatus.setEditable(false);
 		lblStatus.setBackground(SystemColor.menu);
 		lblStatus.setFont(new Font("Arial", Font.PLAIN, 10));
 		lblStatus.setLineWrap(true);
-		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(textAreaPane, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(333)
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 206, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
-								.addComponent(lblStatus)
-								.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-									.addComponent(btnImport, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-									.addComponent(btnRun, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 97, Short.MAX_VALUE)
-									.addComponent(btnSource, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 105, Short.MAX_VALUE))))
-						.addComponent(bottomLabel))
-					.addGap(341))
-		);
-		gl_panel_1.setVerticalGroup(
-			gl_panel_1.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel_1.createSequentialGroup()
-					.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-						.addComponent(textAreaPane, GroupLayout.DEFAULT_SIZE, 224, Short.MAX_VALUE)
-						.addGroup(gl_panel_1.createSequentialGroup()
-							.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING, false)
-								.addGroup(gl_panel_1.createSequentialGroup()
-									.addComponent(btnSource)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(btnImport)
-									.addGap(4)
-									.addComponent(btnRun)
-									.addPreferredGap(ComponentPlacement.RELATED)
-									.addComponent(lblStatus))
-								.addComponent(textArea, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE))
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(bottomLabel)))
-					.addContainerGap())
-		);
-		panel_1.setLayout(gl_panel_1);
+		
+		// set buttons and area in a grid
+		actionPanel = new JPanel(new GridLayout(2, 1, 10, 10));
+		actionPanel.setBackground(Color.pink);
+		actionPanel.add(buttonsPanel);
+		actionPanel.add(lblStatus);
+		
+		// set text area for input
+		textAreaPane = new JScrollPane();
+		textAreaPane.setSize(600,600);
+		textArea = new JTextArea(10,30);
+		
+		textArea.setDropMode(DropMode.INSERT);
+		textArea.setFont(new Font("Calibri Light", Font.PLAIN, 13));
+		textArea.setWrapStyleWord(true);
+		textArea.setLineWrap(true);
+		textArea.setBackground(Color.yellow);
+//		textAreaPane.add(textArea);
+//		textAreaPane.setBackground(Color.green);
+		
+		// set label for hint
+		bottomLabel = new JLabel("Separate IDs from each other by a comma (,).");
+		bottomLabel.setFont(new Font("Calibri Light", Font.PLAIN, 12));
+		bottomLabel.setBackground(Color.magenta);
+		bottomLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		// set text area and label with flow layout
+		bigPanel = new JPanel();
+		bigPanel.setLayout(new BoxLayout(bigPanel, BoxLayout.Y_AXIS));
+		bigPanel.add(textArea);
+		bigPanel.add(bottomLabel);
+		bigPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		
+		// set flowlayout and grid layout into a gridbag
+		
+		// put top label in main panel
+		topLabel = new JLabel("List out designs or item IDs to check which customers have them built out.");
+		
+		// put gridbag layout into main panel
+		
+
+		
+				
+		
+
+		frame.add(bigPanel);
+//		panel_1.add(buttonsPanel, BorderLayout.EAST);
+		
 		
 		btnSource.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -182,6 +190,8 @@ public class App {
 							workbook = new XSSFWorkbook(fis);
 							sheet = workbook.getSheetAt(0);
 							lblStatus.setText("Source selected: "+file.getAbsolutePath());
+							window.frame.update(null);
+							
 						}
 					} catch (Exception err) {
 						err.getStackTrace();
@@ -203,6 +213,13 @@ public class App {
 				
 			}
 		});
+
+		
+		Panel panel_1 = new Panel();
+		panel_1.setSize(frame.size());
+//		frame.getContentPane().add(panel_1);
+		
+		
 		
 		textArea.addFocusListener(new FocusAdapter() {
 			@Override
